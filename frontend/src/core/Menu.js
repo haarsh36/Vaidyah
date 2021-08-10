f06dbd652ee86e3ca56ff1f6aea157d73164f4fc
haarsh36 <61229315+haarsh36@users.noreply.github.com>
2021-08-10 19:06:21 +0530

    Add files via upload

---
+++ frontend/src/core/Menu.js
@@ -0,0 +1,122 @@
+import React, { Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout, isAutheticated } from "../auth/helper";
+
+const currentTab = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#2ecc72" };
+  } else {
+    return { color: "#FFFFFF" };
+  }
+};
+
+const Menu = ({ history }) => (
+  <div>
+    <ul className="nav nav-tabs bg-dark">
+      <li className="nav-item">
+        <Link style={currentTab(history, "/")} className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          style={currentTab(history, "/cart")}
+          className="nav-link"
+          to="/cart"
+        >
+          Cart
+        </Link>
+      </li>
+      {isAutheticated() && isAutheticated().user.role === 0 && (
+        <li className="nav-item">
+          <Link
+            style={currentTab(history, "/user/dashboard")}
+            className="nav-link"
+            to="/user/dashboard"
+          >
+            U. Dashboard
+          </Link>
+        </li>
+      )}
+      {isAutheticated() && isAutheticated().user.role === 1 && (
+        <li className="nav-item">
+          <Link
+            style={currentTab(history, "/admin/dashboard")}
+            className="nav-link"
+            to="/admin/dashboard"
+          >
+            A. Dashboard
+          </Link>
+        </li>
+      )}
+      {!isAutheticated() && (
+        <Fragment>
+          <li className="nav-item">
+            <Link
+              style={currentTab(history, "/signup")}
+              className="nav-link"
+              to="/signup"
+            >
+              Signup
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              style={currentTab(history, "/signin")}
+              className="nav-link"
+              to="/signin"
+            >
+              Sign In
+            </Link>
+          </li>
+        </Fragment>
+      )}
+            {isAutheticated() && isAutheticated().user.role === 0 && (
+        <li className="nav-item">
+          <Link
+            style={currentTab(history, "/user/doctor")}
+            className="nav-link"
+            to="/user/doctor"
+            
+          >
+           Doctor
+          </Link>
+        </li>
+      )}
+      <li className="nav-item">
+        <Link style={currentTab(history, "/medicalstore")} className="nav-link" to="/medicalstore">
+          Medical Store
+        </Link>
+        
+      </li>
+      <li className="nav-item">
+        <a href="https://www.mygov.in/covid-19/" className="nav-link text-white">
+        Co-Stats
+        </a>
+        
+      </li>
+      <li className="nav-item">
+        <a href="http://127.0.0.1:5502/index.html" className="nav-link text-white">
+        Blogs
+        </a>
+        
+      </li>
+      {isAutheticated() && (
+        <li className="nav-item">
+          <span
+            className="nav-link text-warning"
+            onClick={() => {
+              signout(() => {
+                history.push("/");
+              });
+            }}
+          >
+            Signout
+          </span>
+        </li>
+      )}
+    </ul>
+  </div>
+);
+
+export default withRouter(Menu);
